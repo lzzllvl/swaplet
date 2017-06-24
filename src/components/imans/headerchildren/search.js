92a4b47c156cc2f4c4cf2f8663255a18302e0eb9
Iman <imanhamidi91@gmail.com>
2017-06-24 11:03:53 -0700

    IMANS HEADER COMPONENTS

---
+++ src/components/imans/headerchildren/search.js
@@ -0,0 +1,7 @@
+// THIS IS THE SEARCH COMPONENT
+<div class="col-sm-4">
+	<div class="form-group label-floating">
+		<label class="control-label">With Floating Label</label>
+		<input type="email" class="form-control"/>
+	</div>
+</div>
