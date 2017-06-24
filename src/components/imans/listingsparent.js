bca4c35e12dcd7290ba6b7a883e7d6f515412944
Iman <imanhamidi91@gmail.com>
2017-06-24 12:12:30 -0700

    ADDED LISTING PARENT AND CARD CHILD COMPONENT - BOTH NEED EDITS

--- src/components/imans/listingsparent.js
+++ src/components/imans/listingsparent.js
@@ -1,11 +1,15 @@
 // THIS IS THE MAIN CONTAINER THAT SHOWS THE LIST OF LISTINGS
 // FOUND ON THE HOMEPAGE
 // WILL CONTAIN LISTING CARD CHILD COMPONENTS
+// WE ONLY NEED ONE CARD CHILD COMPONENT INPUTTED IN HERE...
+// REACT HAS THE MAP FUNCTION IN THE RENDER TO REUSE THIS COMPONENT FOR MULTIPLE LISTINGS
+// https://facebook.github.io/react/docs/lists-and-keys.html#basic-list-component
 <div className="container">
 	<div className="row">
+		<div class="col-sm-10 col-sm-offest-1">
 
 
 
-
+		</div>
 	</div>
 </div>
