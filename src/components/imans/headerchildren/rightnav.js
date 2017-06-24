7c656708a19149b22ba11f47af66d571f2e9a7e5
oscar luna <os.lun11@gmail.com>
2017-06-24 12:01:28 -0700

    no message

--- src/components/imans/headerchildren/rightnav.js
+++ src/components/imans/headerchildren/rightnav.js
@@ -1,30 +1,40 @@
-// Add this component to the header parent, after the logoseach component
-<div class="collapse navbar-collapse" id="navigation-example">
-    <ul class="nav navbar-nav navbar-right">
-        <li>
-            <a href="../components-documentation.html" target="_blank">
-                Components
-            </a>
-        </li>
-        <li>
-            <a href="http://demos.creative-tim.com/material-kit-pro/presentation.html?ref=utp-freebie" target="_blank">
-                <i class="material-icons">unarchive</i> Upgrade to PRO
-            </a>
-        </li>
-        <li>
-            <a href="https://twitter.com/CreativeTim" target="_blank" class="btn btn-simple btn-white btn-just-icon">
-                <i class="fa fa-twitter"></i>
-            </a>
-        </li>
-        <li>
-            <a href="https://www.facebook.com/CreativeTim" target="_blank" class="btn btn-simple btn-white btn-just-icon">
-                <i class="fa fa-facebook-square"></i>
-            </a>
-        </li>
-        <li>
-            <a href="https://www.instagram.com/CreativeTimOfficial" target="_blank" class="btn btn-simple btn-white btn-just-icon">
-                <i class="fa fa-instagram"></i>
-            </a>
-        </li>
-    </ul>
-</div>
+import React from 'react';
+
+class Rightnav extends React.Component {
+    render() {
+        return (
+
+            // Add this component to the header parent, after the logoseach component
+            <div class="collapse navbar-collapse" id="navigation-example">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="../components-documentation.html" target="_blank">
+                            Components
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://demos.creative-tim.com/material-kit-pro/presentation.html?ref=utp-freebie" target="_blank">
+                            <i class="material-icons">unarchive</i> Upgrade to PRO
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/CreativeTim" target="_blank" class="btn btn-simple btn-white btn-just-icon">
+                            <i class="fa fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.facebook.com/CreativeTim" target="_blank" class="btn btn-simple btn-white btn-just-icon">
+                            <i class="fa fa-facebook-square"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/CreativeTimOfficial" target="_blank" class="btn btn-simple btn-white btn-just-icon">
+                            <i class="fa fa-instagram"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        );
+    } 
+}
+
