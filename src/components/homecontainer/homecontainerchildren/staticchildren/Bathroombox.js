546a1d25e04a190753baed18222461400d3e1571
oscar luna <os.lun11@gmail.com>
2017-06-29 19:42:12 -0700

    import components

--- src/components/homecontainer/homecontainerchildren/staticchildren/Bathroombox.js
+++ src/components/homecontainer/homecontainerchildren/staticchildren/Bathroombox.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
+import Bbox from "../bathboxchildren/Bbox";
+
 class Bathroombox extends React.Component {
     render() {
         return  (
 
-        	    <div className="row">
-        	    	<h4 className="rbc-title col s12 col-xs-12">Bathrooms</h4>
-					<Bbox/>
-				</div>
+	    <div className="row">
+	    	<h4 className="rbc-title col s12 col-xs-12">Bathrooms</h4>
+			<Bbox/>
+		</div>
 
         );
     }
