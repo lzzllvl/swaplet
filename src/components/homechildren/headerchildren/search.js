6dce8484d1aee7912346dfc5f036c1c2ca334abb
Zackery Rilling <rillingz@yahoo.com>
2017-07-07 15:54:56 -0700

    Changes to style and daterange created.

--- src/components/homechildren/headerchildren/search.js
+++ src/components/homechildren/headerchildren/search.js
@@ -6,10 +6,13 @@ class Search extends React.Component {
 	render() {
 		return (
 			// THIS IS THE SEARCH COMPONENT
-			<div className="search" id="search">
+			<div className="search col-sm-4" id="search">
 
-					<button className="btn-sm btn-info" onClick={this.props.handleSubmit}>Search</button>
-					<input type="text" stateName="searchString" className="searchinput" onChange={this.props.handleChange} placeholder="Search a City"/>
+					<div className="logo">Swaplet</div>
+					<div>
+						<input type="text" stateName="searchString" className="searchinput input btn" onChange={this.props.handleChange} placeholder="Destination, City, Address"/>
+						<button className="btn btn-info" onClick={this.props.handleSubmit}>Search</button>
+					</div>
 
 			</div>
 		
