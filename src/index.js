1e49c45d2c2e42f161554cc4989c37addff3f997
oscar luna <os.lun11@gmail.com>
2017-07-06 21:28:24 -0700

    fixed bug

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import UserProfile from './components/imans/UserProfileParent';
+import Home from './components/Home';
 
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 
-ReactDOM.render(<UserProfile />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
